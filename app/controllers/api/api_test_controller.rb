0018978e5eb627a5b5a9055fbd6cc054ab6ffd7c
Paushali <paushali.kundu@mavs.uta.edu>
2016-10-24 20:52:42 -0500

    Changed Gemfile for Heroku

--- app/controllers/api/api_test_controller.rb
+++ app/controllers/api/api_test_controller.rb
@@ -1,9 +1,19 @@
 class  Api::ApiTestController < ApplicationController
   def apitestfunc
     #@jsondata= '{"First Name": "Martha"}'
+    @jsondata= '{"AGAWAM" : 20,
+  "BARRE" : 30,
+  "BELCHERTOWN": 32,
+  "CHESTER":13,
+  "CHICOPEE":27,
+  "MOUNT TOM":53,
+  "GOSHEN":25,
+  "LUDLOW":34,
+  "OTIS":45,
+  "CHARLTON":54}'
     #render :json => @jsondata
     #@jsondata = JSON.parse(File.read('G:/Web Data Management/Ruby Projects/HelloWorld/app/controllers/api/zips.json'))
-    @jsondata = File.read('/api/zips.json')
+    #@jsondata = File.read('/api/zips.json')
     render :json => @jsondata
   end
 end
