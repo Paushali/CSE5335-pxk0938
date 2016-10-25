1729d5d75e8f40902a55f2fd7d4b6cbaf0d9b2ad
Paushali <paushali.kundu@mavs.uta.edu>
2016-10-24 20:46:30 -0500

    Changed back to remote

--- app/controllers/api/api_test_controller.rb
+++ app/controllers/api/api_test_controller.rb
@@ -1,8 +1,9 @@
 class  Api::ApiTestController < ApplicationController
   def apitestfunc
-    @jsondata= '{"First Name": "Martha"}'
-    render :json => @jsondata
-    #@jsondata = JSON.parse(File.read('G:/Web Data Management/Ruby Projects/HelloWorld/app/controllers/api/zips.json'))
+    #@jsondata= '{"First Name": "Martha"}'
     #render :json => @jsondata
+    #@jsondata = JSON.parse(File.read('G:/Web Data Management/Ruby Projects/HelloWorld/app/controllers/api/zips.json'))
+    @jsondata = File.read('G:/Web Data Management/Ruby Projects/HelloWorld/app/controllers/api/zips.json')
+    render :json => @jsondata
   end
 end
